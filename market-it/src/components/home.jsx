341a3e7bf2a0de4cdc43524404b71b291a28fa3b
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-08-04 22:11:14 +0100

    Created the data file - db.json, added the useFetch.js custom hook

--- market-it/src/components/home.jsx
+++ market-it/src/components/home.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import image1 from './images/image-1.png'
+import useFetch from './usefetch';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+    const { data: laptops} = useFetch('http://localhost:8000/laptops/');
   return (
     <div className='home'>
         <div className="container">
@@ -10,10 +13,23 @@ const Home = () => {
                     <p className="main-subsection">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias accusantium impedit quae numquam doloremque quas?</p>
                     <button className="main-button">Shop Now</button>
                 </div>
+                <div className="main-img">
+                    <img src={image1} alt="laptop" height={400} width={600} />
+                </div>
             </div>
             <div className="featured">
                 <h2> Trending Products</h2>
-                
+                <div className="flex">
+                    {laptops && laptops.map((laptop) => (
+                    <div className="col" key={laptop.id}>
+                        {laptop.id < 3 && <Link to={`/products`}>
+                                <img src={laptop.image} alt="" height={200} width={320} />
+                                <h2>{laptop.name}</h2>
+                                <p>$ {laptop.price}</p>
+                        </Link>}
+                    </div>
+                    ))}
+                </div>
             </div>
         </div>
     </div>
