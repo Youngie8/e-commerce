7054861ebfc2c0a9e64625d39a90e4fcaf6dd674
taiwo oluwole-young <taiwooluwoleyoung@gmail.com>
2023-08-16 20:53:04 +0100

    Started a change on the about page

--- market-it/src/components/about.jsx
+++ market-it/src/components/about.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 const About = () => {
   return (
     <div className="about">
-        <h2>About page</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, neque! Laboriosam eveniet, ex inventore nemo non, quidem animi rerum dignissimos, ad reprehenderit quisquam consequatur tempora facere laudantium deserunt aspernatur perspiciatis!</p>
+        <h2>How We Came To Be</h2>
+        <div className="problem">
+          <h2 className="problem-header">The Problem</h2>
+          <p><span className="front-qoute">"</span> I diligently saved funds for an entire year with the intent of purchasing aspecific laptop that my daughter had expressed her desire for. Upon arriving the store, I discovered that the price of the laptop has unexpectedly doubled. This revelation brought me considerable distress. I really wished there was a pay in installments store where I can buy laptops. <span className="back-qoute">"</span></p>
+        </div>
+        <div className="solution">
+          <h2 className="solution-header">Our Response</h2>
+          <p>In Africa, there are so many people who have lost out on devices and probably opportunities indirectly. Thus we decided to create an app-store where you can buy things at once, in installment or you save to buy.</p>
+        </div>
+        
     </div>
   )
 }
