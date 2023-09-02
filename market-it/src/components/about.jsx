a48e23128a5b5d5633374da4faaabc50608fc7c1
unknown <godsfavourtolu@gmail.com>
2023-09-02 02:32:59 +0100

    Modified the notfound component

--- market-it/src/components/about.jsx
+++ market-it/src/components/about.jsx
@@ -12,7 +12,7 @@ const About = (props) => {
             <div className="flex about-flex">
               <div className="problem">
                 <h2 className="problem-header">The Problem</h2>
-                <p><span className="front-qoute">"</span> I diligently saved funds for an entire year with the intent of purchasing aspecific laptop that my daughter had expressed her desire for. Upon arriving the store, I discovered that the price of the laptop has unexpectedly doubled. This revelation brought me considerable distress. I really wished there was a pay in installments store where I can buy laptops. <span className="back-qoute">"</span></p>
+                <p><span className="front-qoute">"</span> I diligently saved funds for an entire year with the intent of purchasing a specific laptop that my daughter had expressed her desire for. Upon arriving the store, I discovered that the price of the laptop has unexpectedly doubled. This revelation brought me considerable distress. I really wished there was a pay in installments store where I can buy laptops. <span className="back-qoute">"</span></p>
               </div>
               <div className="solution">
                 <h2 className="solution-header">Our Response</h2>
--- market-it/src/components/notfound.jsx
+++ market-it/src/components/notfound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
+    <div className='notfound'>
+      <h1>ERROR</h1>
+      <h1 className="err404">404</h1>
+      <p>You don dey go where you no know oo! </p>
+      <Link to="/"><button>Go Home</button></Link>
+    </div>
   )
 }
 
