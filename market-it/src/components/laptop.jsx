15b33d85adcb146b00167356514b3bfad8a035c8
Taiwo <taiwooluwoleyoung@gmail.com>
2024-02-29 22:35:32 +0100

    made updates

--- market-it/src/components/laptop.jsx
+++ market-it/src/components/laptop.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 
 const Laptop = (props) => {
+  
     const laptops = props.laptops;
     const { id } = useParams();
   return (
     <div>
-        { laptops && laptops.map((laptop) => {
+        {/* { laptops && laptops.map((laptop) => {
           <div>
             { <div>
               <h1>{laptop.id}</h1>
               </div>}
           </div>
-        })}
+        })} */}
+
     </div>
   )
 }
